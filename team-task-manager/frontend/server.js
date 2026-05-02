ef6a3d7f8bcf5fae840738949bda2bf5bf759c58
satyamtripathii <satyamtripathi798@gmail.com>
2026-05-02 23:40:12 +0530

    fix express wildcard route

--- team-task-manager/frontend/server.js
+++ team-task-manager/frontend/server.js
@@ -1,22 +1,16 @@
-
-
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-// Railway port
 const PORT = process.env.PORT || 5000;
 
-// Serve static files
 app.use(express.static(path.join(__dirname, "dist")));
 
-// React SPA fallback
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-// Start server
 app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 Server running on port " + PORT);
 });
